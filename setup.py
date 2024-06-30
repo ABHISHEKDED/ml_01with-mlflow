1c4870153a29cb38a7d443a341c203699d52a88a
ABHISHEKDED <singh.abhishek.ctg@gmail.com>
2024-06-30 20:49:04 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "ml_01with-mlflow"
+AUTHOR_USER_NAME = "ABHISHEKDED"
+SRC_REPO = "mlProject"
+AUTHOR_EMAIL = "singh.abhishek.ctg@gmail.com"
+
+
